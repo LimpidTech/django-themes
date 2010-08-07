7fcb9cf0bba864fc024b9c4d25475d07943acd01
Brandon R. Stoner <monokrome@monokro.me>
2010-08-06 21:25:44 -0600

    Modified the model system to use arbitrary theme names when requested.

--- django/contrib/themes/models.py
+++ django/contrib/themes/models.py
@@ -2,29 +2,29 @@ from django.db import models
 from django.conf import settings
 from django.template.defaultfilters import slugify
 
-# TODO: Adapt theme URLs to a dict format.
+# This is a default mapping used when there are no overrides present
+# in the database or in settings
+theme_directories = {
+    'scripts': 'scripts',
+    'images': 'images',
+    'styles': 'styles',
+}
 
-if hasattr(settings, 'THEMES_MEDIA_URL'):
-    THEME_URL = settings.THEMES_MEDIA_URL
-else:
-    THEME_URL = '%sthemes' % settings.MEDIA_URL
+if hasattr(settings, 'THEME_MEDIA_DIRS'):
+    theme_directories.update(settings.THEME_MEDIA_DIRS)
 
-if hasattr(settings, 'THEMES_STYLE_URL'):
-    STYLE_URL = settings.THEMES_STYLE_URL
-else:
-    STYLE_URL = 'styles'
+class ThemeOverride(models.Model):
+    """ Used to override a theme's settings in the database. """
+    name = models.CharField(max_length=16)
+    directory = models.CharField(max_length=16)
+    theme = models.ForeignKey('Theme')
 
-if hasattr(settings, 'THEMES_SCRIPT_URL'):
-    SCRIPT_URL = settings.THEMES_SCRIPT_URL
-else:
-    SCRIPT_URL = 'scripts'
-
-if hasattr(settings, 'THEMES_IMAGE_URL'):
-    IMAGE_URL = settings.THEMES_IMAGE_URL % THEME_URL
-else:
-    IMAGE_URL = 'images'
+    def __unicode__(self):
+        return self.name
 
 class Theme(models.Model):
+    """ Represents a theme (aka skin) that can be applied to your
+        django application. """
     name = models.CharField(max_length=32)
     directory = models.SlugField(max_length=32, null=True, blank=True)
     default = models.BooleanField(default=False)
@@ -32,14 +32,19 @@ class Theme(models.Model):
     def base_url(self):
         return '%s/%s' % (THEME_URL, self.directory)
 
-    def style_url(self):
-        return '%s/%s' % (self.base_url(), STYLE_URL)
+    def media_url(self, media_type):
+        media_dir = media_type
 
-    def script_url(self):
-        return '%s/%s' % (self.base_url(), SCRIPT_URL)
+        if media_type in theme_directories:
+            media_dir = theme_directories[media_type]
 
-    def image_url(self):
-        return '%s/%s' % (self.base_url(), IMAGE_URL)
+        try:
+            override = ThemeOverride.objects.get(theme=self,name=media_type)
+            media_dir = override.value
+        except ThemeOverride.DoesNotExist:
+            pass
+
+        return '%s/%s' % (self.base_url(), media_type)
 
     def save(self, force_insert=False, force_update=False):
         if self.default == True:
