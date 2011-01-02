ad3d37d322b8a83c6f464dd35eb38e605a6df3f4
Brandon R. Stoner <github@monokro.me>
2011-01-02 08:47:23 -0800

    Modified signal receiver to remove instance from the offending themes list

--- themes/signals.py
+++ themes/signals.py
@@ -5,7 +5,9 @@ from themes.models import Theme
 @receiver(post_save, sender=Theme)
 def post_save_handler(sender, instance, **kwargs):
     for site in instance.sites_enabled.all():
-        offending_themes = Theme.objects.filter(sites_enabled=site)
+        offending_themes = list(Theme.objects.filter(sites_enabled=site))
+
+        offending_themes.remove(instance)
 
         for theme in offending_themes:
             theme.sites_enabled.delete(site)
